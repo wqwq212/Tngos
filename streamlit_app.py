5905036ddba140525e44624e697a5df5b4f30c14
wqwq212 <20242724@sdhs.gwe.hs.kr>
2024-12-13 15:51:20 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import gdown
 file_id = '1s12QBURrTDKEXdfwDN7_AvchfUxCB9Ch'
 
 # Google Drive에서 파일 다운로드 함수
-@st.cache(allow_output_mutation=True)
+@st.cache_data(allow_output_mutation=True)
 def load_model_from_drive(file_id):
     url = f'https://drive.google.com/uc?id={file_id}'
     output = 'model.pkl'
